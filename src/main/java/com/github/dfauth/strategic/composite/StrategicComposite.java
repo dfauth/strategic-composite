4f414d849678780c87a3e9c64f2d6e8c4bfd3608
damian <dfauth@gmail.com>
2020-11-24 23:27:38 +0800

    initial commit

--- src/main/java/com/github/dfauth/strategic/composite/StrategicComposite.java
+++ src/main/java/com/github/dfauth/strategic/composite/StrategicComposite.java
@@ -3,8 +3,129 @@ package com.github.dfauth.strategic.composite;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class StrategicComposite {
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StrategicComposite<T> {
 
     private static final Logger logger = LoggerFactory.getLogger(StrategicComposite.class);
 
+    private final Strategy strategy;
+    private final List<T> clients;
+    private int current = 0;
+
+    public static <T> StrategicComposer<T> createCompositeOfType(Class<T> classOfT) {
+        return new StrategicComposer<T>(classOfT);
+    }
+
+    public StrategicComposite(List<T> clients) {
+        this(new StickyStrategy(clients.size()), clients);
+    }
+
+    public StrategicComposite(Strategy strategy, List<T> clients) {
+        this.strategy = strategy;
+        this.clients = clients;
+    }
+
+    private T current() {
+        return current(0);
+    }
+
+    private T current(int i) {
+        return clients.get(i%clients.size());
+    }
+
+    public T getComposite(Class<T> cls) {
+        return (T) Proxy.newProxyInstance(this.getClass().getClassLoader(),
+                new Class[]{cls}, (proxy, method, args) -> getComposite(current, method, args));
+    }
+
+    private T getComposite(final int i, Method method, Object[] args) {
+        if(i>= clients.size()) {
+            throw new RuntimeException("Oops, no available implementation");
+        }
+        return (T) findMethod(current(i), method).map(m -> {
+            try {
+                T result = (T) m.invoke(current(i), args);
+                current = strategy.success(current);
+                return result;
+            } catch (IllegalAccessException e) {
+                logger.info(e.getMessage(), e);
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                logger.info(e.getMessage(), e);
+                current = strategy.failure(e.getCause(), i);
+                return getComposite(strategy.get(current), method, args);
+            }
+        }).orElseThrow(() ->
+                new RuntimeException("No method found: "+method.getName()));
+    }
+
+    private Optional<Method> findMethod(T current, Method method) {
+        return Stream.of(current.getClass().getMethods()).filter(m ->
+                m.getName().equals(method.getName()) &&
+                        m.getReturnType().equals(method.getReturnType()) &&
+                        Arrays.equals(m.getParameterTypes(), method.getParameterTypes())
+        ).findFirst();
+    }
+
+    public interface Strategy {
+
+        int failure(Throwable t, int i);
+
+        int get(int current);
+
+        int success(int current);
+    }
+
+    public static class StickyStrategy implements Strategy {
+
+        private final int size;
+
+        public StickyStrategy(int size) {
+            this.size = size;
+        }
+
+        @Override
+        public int failure(Throwable t, int i) {
+            return i+1%size;
+        }
+
+        @Override
+        public int get(int i) {
+            return i%size;
+        }
+
+        @Override
+        public int success(int i) {
+            return i;
+        }
+    }
+
+    public static class StrategicComposer<T> {
+
+        private final Class<T> classOfT;
+        private Optional<Strategy> optStrategy = Optional.empty();
+
+        public StrategicComposer(Class<T> classOfT) {
+            this.classOfT = classOfT;
+        }
+
+        public StrategicComposer withStrategy(Strategy strategy) {
+            this.optStrategy = Optional.ofNullable(strategy);
+            return this;
+        }
+
+        public T compose(T... arrayOfT) {
+            return optStrategy
+                    .map(s -> new StrategicComposite<T>(s, Stream.of(arrayOfT).collect(Collectors.toList())).getComposite(classOfT))
+                    .orElse(new StrategicComposite<T>(Stream.of(arrayOfT).collect(Collectors.toList())).getComposite(classOfT));
+        }
+    }
 }
--- src/test/java/com/github/dfauth/strategic/composite/TestCase.java
+++ src/test/java/com/github/dfauth/strategic/composite/TestCase.java
@@ -1,10 +1,81 @@
 package com.github.dfauth.strategic.composite;
 
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkException;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.JwkProviderBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.concurrent.Callable;
+
+import static com.github.dfauth.strategic.composite.StrategicComposite.createCompositeOfType;
+import static com.github.dfauth.trycatch.TryCatch.tryCatch;
+import static org.testng.Assert.assertNotNull;
 
 public class TestCase {
 
     private static final Logger logger = LoggerFactory.getLogger(TestCase.class);
 
+    @Test
+    public void testJwkProvider() {
+        try {
+            JwkProvider provider = new JwkProviderBuilder("http://localhost:8080").build();
+            JwkProvider mock = new MockJwkProvider();
+            JwkProvider composed = createCompositeOfType(JwkProvider.class).compose(provider, mock);
+            logger.info("first run took "+timed(() -> {
+                Jwk mockResult = composed.get("blah");
+                assertNotNull(mockResult);
+                return mockResult;
+            }).elapsed()+" msec");
+            logger.info("second run took "+timed(() -> {
+                Jwk mockResult = composed.get("blah");
+                assertNotNull(mockResult);
+                return mockResult;
+            }).elapsed()+" msec");
+        } catch (Exception e) {
+            logger.info(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private TimedResult<Void> timed(Runnable r) {
+        return timed(() -> {
+            r.run();
+            return null;
+        });
+    }
+
+    interface TimedResult<T> {
+        long elapsed();
+        T result();
+    }
+
+    private <T> TimedResult<T> timed(Callable<T> c) {
+        return tryCatch(() -> {
+            long now = System.currentTimeMillis();
+            T result = c.call();
+            long elapsed = System.currentTimeMillis() - now;
+            return new TimedResult<T>() {
+                @Override
+                public long elapsed() {
+                    return elapsed;
+                }
+
+                @Override
+                public T result() {
+                    return result;
+                }
+            };
+        });
+    }
+
+    public static class MockJwkProvider implements JwkProvider {
+        @Override
+        public Jwk get(String keyId) throws JwkException {
+            return new Jwk("kid", "kty", "alg", "use", Collections.emptyList(), "x5u", Collections.emptyList(), "x5t", Collections.emptyMap());
+        }
+    }
 }
