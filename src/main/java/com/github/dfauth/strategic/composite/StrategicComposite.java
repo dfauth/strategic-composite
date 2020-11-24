943caee6543bb68d93ec22ac89ff99fa06ffb7b6
damian <dfauth@gmail.com>
2020-11-24 22:52:33 +0800

    initial commit

---
+++ src/main/java/com/github/dfauth/strategic/composite/StrategicComposite.java
@@ -0,0 +1,10 @@
+package com.github.dfauth.strategic.composite;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StrategicComposite {
+
+    private static final Logger logger = LoggerFactory.getLogger(StrategicComposite.class);
+
+}
